fde5e4b16948b176fab8753cb07328178e6aa7d3
Tatanelprr <ethan.lepareur@gmail.com>
2024-05-27 19:41:31 +0200

    debutframe

--- Controleur.java
+++ Controleur.java
@@ -1,5 +1,7 @@
 import java.util.ArrayList;
 import java.util.List;
+import javax.swing.*;
+import java.awt.*;
 
 public class Controleur 
 {
@@ -8,20 +10,43 @@ public class Controleur
 
     public Controleur()
     {
+        JFrame    frame      = new JFrame("Mille Sabords");
+        Dimension screenSize = Toolkit.getDefaultToolkit().getScreenSize();
+        
         for (Joueur joueur : jeu.getJoueurs()) 
         {
             List<Object> joueurInfo = new ArrayList<>();
             joueurInfo.add(joueur.getNom());
             joueurInfo.add(joueur.getScore());
             this.tabScore.add(joueurInfo);
-        }
+        }  
 
+        System.out.println();
         System.out.println("Les joueurs sont : ");
-        
+
         for (List<Object> joueurInfo : this.tabScore) 
         {
             System.out.println("Nom: " + joueurInfo.get(0) + ", Score: " + joueurInfo.get(1));
         }
+
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        frame.setSize(screenSize.width, screenSize.height);
+
+        ImageIcon imageIcon = new ImageIcon("images/fond.jpg");
+        Image image         = imageIcon.getImage();
+    
+        JPanel mainPanel = new JPanel()
+        {
+            protected void paintComponent(Graphics g) 
+            {
+                super.paintComponent(g);
+                g.drawImage(imageIcon.getImage(), 0, 0, getWidth(), getHeight(), this);
+            }
+        };
+
+        frame.add(mainPanel);
+        
+        frame.setVisible(true);
     }
 
 
